f8b11fbf8f4771e6c1d6e1e7e340933e44a2cdd3
Jim Ockers <jim.ockers@gmail.com>
2014-07-25 18:25:02 -0400

    Add plot1.R (incomplete) -- reads data files

---
+++ plot1.R
@@ -0,0 +1,31 @@
+## plot1.R
+
+## Make a plot showing the total PM2.5 emission from all sources for
+## each of the years 1999, 2002, 2005, and 2008
+
+## fetch zip file from URL (if required)
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+localFilename <- "FNEI_data.zip"
+
+if (!file.exists(localFilename)) {
+    print("Fetching data set from URL...")
+    download.file(fileUrl, destfile = localFilename, mode = "wb")
+} else {
+    print("Using local copy of data set.")
+}
+
+dataFileList <- unzip(localFilename,list = TRUE)
+
+if (!file.exists("data")) {  
+    unzip(localFilename, overwrite=FALSE, exdir="data")
+}
+
+## Read two files
+
+if (!exists("NEI")) {
+    NEI <- readRDS("./data/summarySCC_PM25.rds")
+}
+if (!exists("SCC")) {
+    SCC <- readRDS("./data/Source_Classification_Code.rds")
+}
