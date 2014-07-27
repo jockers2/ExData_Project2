70e206bb45972cb612c775f6f30e646408eb9413
Jim Ockers <jim.ockers@gmail.com>
2014-07-27 14:08:50 -0400

    Completed plot4.R and added png output

--- plot4.R
+++ plot4.R
@@ -15,13 +15,16 @@ if (!file.exists(localFilename)) {
     print("Using local copy of data set.")
 }
 
+## unzip the file to the ./data directory (if required)
+
 dataFileList <- unzip(localFilename,list = TRUE)
 
 if (!file.exists("data")) {  
     unzip(localFilename, overwrite=FALSE, exdir="data")
 }
 
-## Read two files
+## Read the two files if they are not already in the R
+## environment (saves time to check first)
 
 if (!exists("NEI")) {
     NEI <- readRDS("./data/summarySCC_PM25.rds")
@@ -30,10 +33,9 @@ if (!exists("SCC")) {
     SCC <- readRDS("./data/Source_Classification_Code.rds")
 }
 
-## which SCC ids are related to coal combustion?
+## Which SCC ids are related to coal combustion?
 
 coal_sccs <- SCC$SCC[grep("coal|Coal",SCC$Short.Name,value=FALSE)]
-
 NEI_coal <- NEI[NEI$SCC %in% coal_sccs, ]
 
 # generate summary by year
@@ -43,7 +45,55 @@ coalEmissionsByYear <- ddply(NEI_coal, c("year"), summarise,
                              total.Emissions=sum(Emissions),
                              median.Emissions=median(Emissions), 
                              number.Readings=length(Emissions) )
+years < unique(coalEmissionsByYear$year)
 
-with(coalEmissionsByYear, plot(year,median.Emissions))
-model <- lm(median.Emissions ~ year, coalEmissionsByYear)
-abline(model, lwd=2)
+## Multiple plots
+par(mfrow = c(1,2))
+par(ann=FALSE)
+
+## Plot4a Total coal based emissions over Time
+
+with(coalEmissionsByYear, plot(year,total.Emissions, xaxt="n"))
+axis(1,at=years,labels=years)
+model4a <- lm(total.Emissions ~ year, coalEmissionsByYear)
+abline(model4a, lwd=2)
+title("Total Coal Based Emissions over Time\nacross United States",
+      ylab = "Total Emissions (tons)",
+      xlab = "Year")
+
+#Plot4b Median coal based emissions over Time
+
+with(coalEmissionsByYear, plot(year,median.Emissions,xaxt="n"))
+axis(1,at=years,labels=years)
+model4b <- lm(median.Emissions ~ year, coalEmissionsByYear)
+abline(model4b, lwd=2)
+title("Coal Based Emissions over Time\nacross United States",
+      ylab = "Median Emissions (tons)",
+      xlab = "Year")
+
+## create plot directly on png device
+
+png("plot4.png",width=2*480)
+    par(mfrow = c(1,2))
+    par(ann=FALSE)
+
+    ## Plot4a
+
+    with(coalEmissionsByYear, plot(year,total.Emissions, xaxt="n"))
+    axis(1,at=years,labels=years)
+    model4a <- lm(total.Emissions ~ year, coalEmissionsByYear)
+    abline(model4a, lwd=2)
+    title("Total Coal Based Emissions over Time\nacross United States",
+          ylab = "Total Emissions (tons)",
+          xlab = "Year")
+
+    #Plot4b
+    with(coalEmissionsByYear, plot(year,median.Emissions,xaxt="n"))
+    axis(1,at=years,labels=years)
+    model4b <- lm(median.Emissions ~ year, coalEmissionsByYear)
+    abline(model4b, lwd=2)
+    title("Coal Based Emissions over Time\nacross United States",
+          ylab = "Median Emissions (tons)",
+          xlab = "Year")
+
+dev.off()
